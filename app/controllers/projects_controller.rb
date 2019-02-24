7bb86688d7b62d42ceef97fa5bbf16073aa4e54b
Hangula <Hangula.luk@gmail.com>
2019-02-24 19:26:00 +0200

    Add New and create actions with view files

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,20 @@ class ProjectsController < ApplicationController
 		
 	end
 
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(play_params)
+	end
+
+	private
+
+	def play_params
+		params.require(:project).permit(:title, :info, :level)
+	end
+
 end
+
+
